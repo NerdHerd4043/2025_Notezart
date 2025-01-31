fa139e1b4077f4397ee7c983a8689e9ff0c93f2f
Senor-Pie <espenpbock@gmail.com>
2025-01-31 00:48:49 -0800

    1/31 Espen - Limelight Based IDs in LimelightUtil

--- src/main/java/frc/robot/util/LimelightUtil.java
+++ src/main/java/frc/robot/util/LimelightUtil.java
@@ -6,29 +6,42 @@ package frc.robot.util;
 
 import java.util.Map;
 
+import frc.robot.util.LimelightHelpers.RawFiducial;
+
 /** Add your docs here. */
 public class LimelightUtil {
     public double offset;
-    public double ID;
+    public int ID;
+    public int myID;
 
-    private Map<Double, OffsetRatios> IDRatios = Map.of(
-            3.0, new OffsetRatios(1.0, 3.0)); // Imagine there are more items in the map
+    private Map<Integer, OffsetRatios> IDRatios = Map.of(
+            3, new OffsetRatios(1.0, 3.0),
+            4, new OffsetRatios(3, 2)); // Imagine there are more items in the map
 
     public double getTargetX(double offset) {
         this.offset = offset;
-        this.ID = LimelightHelpers.getFiducialID("limelight1");
+        this.ID = getID();
 
         final double xRatio = this.IDRatios.get(this.ID).xRatio;
 
-        return (this.offset * xRatio);
+        return this.offset * xRatio;
     }
 
     public double getTargetY(double offset) {
         this.offset = offset;
-        this.ID = LimelightHelpers.getFiducialID("limelight1");
+        this.ID = getID();
 
         final double yRatio = this.IDRatios.get(this.ID).yRatio;
 
-        return (this.offset * yRatio);
+        return this.offset * yRatio;
+    }
+
+    public int getID() {
+        RawFiducial[] fiducials = LimelightHelpers.getRawFiducials("limelight-one");
+        for (RawFiducial fiducial : fiducials) {
+            int myID = fiducial.id;
+        }
+
+        return myID;
     }
 }
